ac7f3760ede6eef1cf04e859eb661447af39e3c7
Andy Hanson <andy.pj.hanson@gmail.com>
2015-11-22 22:24:34 -0800

    Use refactored compiler (`compile` -> `new Compiler(...).compile`)

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 'use strict'
 
 const
-	compileWarnAndThrow = require('mason-node-util/dist/compile-warn-and-throw').default,
 	convertSourceMap = require('convert-source-map'),
-	readFile = require('fs-promise').readFile
+	readFile = require('fs-promise').readFile,
+	Compiler = require('mason-compile/dist/Compiler').default,
+	compileWarnAndThrow = require('mason-node-util/dist/compile-warn-and-throw').default
 
 module.exports = {
 	name: 'mason',
 	inputFormats: ['ms'],
 	outputFormat: 'js',
 	renderFileAsync(filename, options) {
-		return readFile(filename, 'utf-8').then(src => {
+		if (options === null)
+			options = undefined
+		const compiler = new Compiler(options)
+		return readFile(filename, 'utf-8').then(msCode => {
 			// TODO:ES6 const {code, sourceMap} = ...
-			const _ = compileWarnAndThrow(src, filename, options)
+			const _ = compileWarnAndThrow(compiler, msCode, filename)
 			return `${_.code}\n${convertSourceMap.fromObject(_.sourceMap).toComment()}`
 		})
 	}
